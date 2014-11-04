8ee0b98d0863d8b8a3e310c96be086ca1bcd7be3
Brady Garvin <brady.j.garvin@gmail.com>
2014-11-03 20:48:55 -0600

    Added prices variable in a separate file.

---
+++ prices.js
@@ -0,0 +1,38 @@
+var prices = [
+    {
+	name: 'Item',
+	price: 1.99,
+    },
+    {
+	name: 'Item',
+	price: 1.99,
+    },
+    {
+	name: 'Item',
+	price: 1.99,
+    },
+    {
+	name: 'Item',
+	price: 1.99,
+    },
+    {
+	name: 'Item',
+	price: 1.99,
+    },
+    {
+	name: 'Item',
+	price: 1.99,
+    },
+    {
+	name: 'Item',
+	price: 1.99,
+    },
+    {
+	name: 'Item',
+	price: 1.99,
+    },
+    {
+	name: 'Item',
+	price: 1.99,
+    },
+];
